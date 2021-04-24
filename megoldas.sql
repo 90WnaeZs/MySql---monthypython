88b9a54adf0ed874cbfb05d862eaeb7a92026ee8
wayjay90 <42623973+wayjay90@users.noreply.github.com>
2021-04-24 18:48:11 +0200

    Add files via upload

---
+++ megoldas.sql
@@ -0,0 +1,31 @@
+﻿/* 1. feladat:  */
+CREATE DATABASE monthypython
+CHARACTER SET utf8mb4 COLLATE utf8mb4_hungarian_ci;
+
+/* 3. feladat:  */
+SELECT nev 
+FROM `epizodok` 
+WHERE sorozat='1/5';
+
+/* 4. feladat:  */
+SELECT COUNT(*) AS 'epizódok száma' FROM `epizodok` WHERE 1;
+
+/* 5. feladat:  */
+SELECT DISTINCT(szinesz) 
+FROM `forgatokonyv` 
+WHERE szinesz IS NOT NULL
+GROUP BY szinesz
+ORDER BY szinesz ASC;
+
+/* 6. feladat:  */
+SELECT reszletek 
+FROM `forgatokonyv` 
+WHERE szinesz="John Cleese" AND resz="Italian lesson";
+
+/* 7. feladat:  */
+SELECT szinesz AS 'színész',COUNT(reszletek) AS 'bejegyzések száma'
+FROM `forgatokonyv` 
+WHERE szinesz IS NOT NULL
+GROUP BY szinesz
+ORDER BY COUNT(reszletek) DESC
+LIMIT 1;
